72361490497ab81b686542fda57ae17c4acaaf92
Linus Berg <linusberg@localhost.localdomain>
2020-04-18 09:14:05 +0200

    Style fixes.

--- target.js
+++ target.js
@@ -12,14 +12,26 @@ function GetIdx(col, row) {
   return `${col}x${row}`; 
 }
 
+const PRINT_DIV = '#target-print';
 
-const builder = new TargetBuilder('#target-print');
+const builder = new TargetBuilder(PRINT_DIV);
+d3.select('#bg_color').on('input', function() {
+  d3.select(PRINT_DIV).style('background-color', this.value);
+});
 
-d3.select('#add-target').on('click', function() {
-  const size = parseInt(d3.select('#size').node().value);
-  const col = parseInt(d3.select('#column').node().value);
-  const row = parseInt(d3.select('#row').node().value);
-  const color = d3.select('#color').node().value;
+d3.select('#target-add').on('click', function() {
+  const size = parseFloat(d3.select('#size').property('value'));
+ 
+  const col = parseInt(d3.select('#col').property('value'));
+  const row = parseInt(d3.select('#row').property('value'));
+  
+  const grid_color = d3.select('#grid_color').property('value');
+  
+  const c_color = d3.select('#c_color').property('value');
+  const c_size = parseFloat(d3.select('#c_size').property('value'));
+  
+  const txt = d3.select('#txt').property('checked');
+  
   const idx = GetIdx(col, row);
   
   if (idx in targets) {
@@ -29,10 +41,10 @@ d3.select('#add-target').on('click', function() {
   targets[idx] = {
     idx: idx,
     size: size,
-    color: '#000000',
-    be: color,
-    be_size: 2,
-    txt: false,
+    color: grid_color,
+    be: c_color,
+    be_size: c_size,
+    txt: txt,
     metric: true,
     row: row,
     col: col,
